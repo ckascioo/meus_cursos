1681a05b74f5c8f7a587a95b7ec58b7948348a99
Cassio Campos <68764753+ckascioo@users.noreply.github.com>
2023-11-14 12:46:29 -0300

    update

--- projeto_meus_cursos/settings.py
+++ projeto_meus_cursos/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'projeto_meus_cursos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres', 
+        'PASSWORD': '1O9q0QELQ1L5Jlbe',
+        'HOST': 'db.dyiiwhbkkfknskspegfk.supabase.co', 
+        'PORT': '5432'
     }
 }
 

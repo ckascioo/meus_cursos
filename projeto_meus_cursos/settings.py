f8e70d2dc57c4aa980851d04cbd8fcfc0b88a9e5
Cassio Campos <68764753+ckascioo@users.noreply.github.com>
2023-11-14 12:55:29 -0300

    update

--- projeto_meus_cursos/settings.py
+++ projeto_meus_cursos/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0gh8q9br#=mw73bj^nzep)tl6h@8e^p@4m-#$wv_)!s+fe!)b@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False # False para nao mostrar mensagens de erro
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
